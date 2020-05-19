2952fab4f36f4fd10ee99ed15c30ef9e07b49ceb
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-05-19 17:47:29 +0900

    modify domain

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Firebase Handson',
   themeConfig: {
-    domain: 'firebase-authentication-handson.ozaki25.now.sh',
+    domain: 'https://firebase-authentication-handson.ozaki25.now.sh',
     sidebar: ['/page1', '/page2', '/page3', '/page4'],
   },
   markdown: {
